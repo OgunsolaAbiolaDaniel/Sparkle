7723dec76cb0809de966aa6dacd18dda1c4b244d
Abiola <danabiola300@gmail.com>
2024-04-18 13:59:34 +0100

    change of plans

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Header from "@/component/Header/Header";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
     <>
-      <Header />
+     
     </> 
   );
 }
